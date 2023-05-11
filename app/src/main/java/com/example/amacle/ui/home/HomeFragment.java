16dd897ad1c1d9abc44091a2cd7c31b534a02974
arun sharma <arun123gulshan@gmail.com>
2023-05-11 12:06:30 +0530

    fjdhferufrhufnrkoiruhfge3ud

--- app/src/main/java/com/example/amacle/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/amacle/ui/home/HomeFragment.java
@@ -1,86 +1,56 @@
 package com.example.amacle.ui.home;
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.example.amacle.R;
 import com.example.amacle.databinding.FragmentHomeBinding;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import java.util.ArrayList;
 import java.util.List;
-
 public class HomeFragment extends Fragment {
-
     private FragmentHomeBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-
-        LineChart chart = binding.chart;
-
-        List<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(0f, 1.0f));
-        entries.add(new Entry(1f, 2.0f));
-        entries.add(new Entry(2f, 3.5f));
-        entries.add(new Entry(3f, 2.5f));
-        entries.add(new Entry(4f, 4.0f));
-        Legend legend = chart.getLegend();
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        legend.setDrawInside(false);
-        legend.setFormSize(8f);
-        legend.setTextSize(12f);
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
-        LineData lineData = new LineData(dataSet);
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setTextSize(12f);
-        xAxis.setDrawGridLines(false);
-
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setTextSize(12f);
-        leftAxis.setAxisMinimum(0f);
-
-        YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-        Description description = new Description();
-        description.setText("My Fancy Graph");
-        description.setTextSize(16f);
-        description.setPosition(0.5f, 0.95f);
-
-        chart.setDescription(description);
-
+        LineChart chart = root.findViewById(R.id.chart);
+        ArrayList<Entry> data1 = new ArrayList<>();
+        data1.add(new Entry(0, 4));
+        data1.add(new Entry(1, 8));
+        data1.add(new Entry(2, 6));
+        data1.add(new Entry(3, 2));
+        data1.add(new Entry(4, 7));
+        ArrayList<Entry> data2 = new ArrayList<>();
+        data2.add(new Entry(0, 2));
+        data2.add(new Entry(1, 5));
+        data2.add(new Entry(2, 3));
+        data2.add(new Entry(3, 6));
+        data2.add(new Entry(4, 4));
+        LineDataSet dataSet1 = new LineDataSet(data1, "Line 1");
+        dataSet1.setColor(Color.BLUE);
+        dataSet1.setLineWidth(2f);
+        dataSet1.setValueTextColor(Color.BLACK);
+        LineDataSet dataSet2 = new LineDataSet(data2, "Line 2");
+        dataSet2.setColor(Color.RED);
+        dataSet2.setLineWidth(2f);
+        dataSet2.setValueTextColor(Color.BLACK);
+        List<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSet1);
+        dataSets.add(dataSet2);
+        LineData lineData = new LineData(dataSets);
         chart.setData(lineData);
         chart.invalidate();
-        return root;
-    }
-
+        return root;}
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-}
+    public void onDestroyView(){super.onDestroyView();binding = null;}}
